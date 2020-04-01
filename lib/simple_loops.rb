bc42c2ae8c6dce34eefe67c37d2084018cb21914
Issa Sangare <issabmsangare@hotmail.com>
2020-04-01 22:55:49 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,4 +7,10 @@ def loop_message_five_times(message)
   end
    end
    
-def 
+def loop_message_five_times(message, limit)
+  count= 0 
+  while count<5 do
+    puts message
+    count+=1
+  end
+   end
